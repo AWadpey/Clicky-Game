e832ef3935171e3496438ffd063f1b64d170842f
MistahSanD <wadpey@gmail.com>
2019-01-23 23:19:23 -0800

    Updates

---
+++ precache-manifest.ba0be82e6ec3f7f9632bf65fa92d31d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "aa1dbf11d41a584f97a9",
+    "url": "/Clicky-Game/static/js/main.aa1dbf11.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "aa1dbf11d41a584f97a9",
+    "url": "/Clicky-Game/static/css/main.148aee3d.chunk.css"
+  },
+  {
+    "revision": "d69ce07e43283448bc94fc59228eb4ea",
+    "url": "/Clicky-Game/index.html"
+  }
+];
